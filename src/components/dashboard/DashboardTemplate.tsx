c6c67709d7d7cf7330c97c1077176e9500173cbc
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-09-28 23:01:46 +0800

    DMA-13 | Implement automation auth process

--- src/components/dashboard/DashboardTemplate.tsx
+++ src/components/dashboard/DashboardTemplate.tsx
@@ -1,20 +1,58 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import DashboardNavigator from './DashboardNavigator';
 import appTheme, { Color, DRAWER_WIDTH } from '../../constants/appTheme';
 import { useMediaQuery } from '@mui/material';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import DashboardHeader from './DashboardHeader';
 import DashboardFooter from './DashboardFooter';
+import { useAppDispatch } from '../../store/index.store';
+import { verifyToken } from '../../store/loginSlice/login.thunk';
+import ToastifyController from '../../utils/toastifyController';
+import ErrorHandler from '../../utils/errorHandler';
+import { loadingSliceActions } from '../../store/loadingSlice/loading.slice';
 
 const DashboardTemplate: FC = () => {
     const [mobileOpen, setMobileOpen] = useState(false);
     const isSmUp = useMediaQuery(appTheme.breakpoints.up('sm'));
 
+    const navigate = useNavigate();
+
+    const dispatch = useAppDispatch();
+
+    const { openLoading, closeLoading } = loadingSliceActions;
+
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
+
+    useEffect(() => {
+        dispatch(openLoading());
+
+        dispatch(verifyToken())
+            .unwrap()
+            .then((isAuth) => {
+                if (!isAuth) {
+                    ToastifyController.activeError(
+                        'Token is invalid or expired. Please login again.'
+                    );
+
+                    navigate('/login');
+                }
+            })
+            .catch((error) => {
+                if (error.message) {
+                    ErrorHandler.activeToast(new Error(error.message));
+                } else {
+                    ErrorHandler.activeToast(error);
+                }
+            })
+            .finally(() => {
+                dispatch(closeLoading());
+            });
+    }, [dispatch, navigate, openLoading, closeLoading]);
+
     return (
         <>
             <Box sx={{ display: 'flex', minHeight: '100vh' }}>
