5773a37eaebecc0aa7a022c7eeb5d3990e81405b
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-10-03 09:50:36 +0800

    DMA-12 | Add production backend api

--- src/constants/backendApi.ts
+++ src/constants/backendApi.ts
@@ -1,1 +1,4 @@
-export const BACKEND_API = 'http://localhost:3001';
+export const BACKEND_API =
+    import.meta.env.NODE_ENV === 'production'
+        ? 'http://dma-alb-1252925442.ap-southeast-1.elb.amazonaws.com'
+        : 'http://localhost:3001';
