75411d9e05a750031ee79f09a8a5003ca9c0e3a7
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2023-10-14 18:25:29 +0800

    DMA-16 | Remove defaultWeeklyTimeSlots from employeeSlice

--- src/store/employeeSlice/employee.slice.ts
+++ src/store/employeeSlice/employee.slice.ts
@@ -2,25 +2,13 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import SliceName from '../sliceName';
 import { IEmployee } from '../../models/employee/employee.model';
 import { employeeApi } from './employee.api';
-import { employeeTimeSlotApi } from '../api/employeeTimeSlot.api';
-import { IEmployeeDefaultWeeklyTimeSlots } from '../../models/timeSlot/employeeTimeSlot.model';
 
 interface EmployeeState {
     employees: IEmployee[];
-    employeeDefaultWeeklyTimeSlots: IEmployeeDefaultWeeklyTimeSlots;
 }
 
 const initialState: EmployeeState = {
     employees: [],
-    employeeDefaultWeeklyTimeSlots: {
-        mon: null,
-        tue: null,
-        wed: null,
-        thu: null,
-        fri: null,
-        sat: null,
-        sun: null,
-    },
 };
 
 const setEmployees = (
@@ -30,45 +18,21 @@ const setEmployees = (
     state.employees = action.payload;
 };
 
-const clearTimeSlots = (state: EmployeeState) => {
-    state.employeeDefaultWeeklyTimeSlots = {
-        mon: null,
-        tue: null,
-        wed: null,
-        thu: null,
-        fri: null,
-        sat: null,
-        sun: null,
-    };
-};
-
 const employeeSlice = createSlice({
     name: SliceName.EMPLOYEE,
     initialState,
     reducers: {
         setEmployees,
-        clearTimeSlots,
     },
     extraReducers: (builder) => {
-        builder
-            .addMatcher(
-                employeeApi.endpoints.getAllEmployees.matchFulfilled,
-                (state, action) => {
-                    if (action.payload.isSuccess) {
-                        state.employees = action.payload.data;
-                    }
-                },
-            )
-            .addMatcher(
-                employeeTimeSlotApi.endpoints.getEmployeeDefaultWeeklyTimeSlots
-                    .matchFulfilled,
-                (state, action) => {
-                    if (action.payload.isSuccess) {
-                        state.employeeDefaultWeeklyTimeSlots =
-                            action.payload.data;
-                    }
-                },
-            );
+        builder.addMatcher(
+            employeeApi.endpoints.getAllEmployees.matchFulfilled,
+            (state, action) => {
+                if (action.payload.isSuccess) {
+                    state.employees = action.payload.data;
+                }
+            },
+        );
     },
 });
 
